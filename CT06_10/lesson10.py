757cb761368b346af292f44930d12092833f650e
computhink-example <computhink-example@outlook.com>
2026-03-22 00:24:14 +0000

    Update

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,1 +1,13 @@
-print("Hello from lesson 10")
+import turtle
+
+# Setup
+screen = turtle.Screen()
+t = turtle.Turtle()
+t.shape("turtle")
+t.color("cyan")
+
+# Draw a circle
+t.circle(100)
+
+# This keeps the window open!
+turtle.done()
